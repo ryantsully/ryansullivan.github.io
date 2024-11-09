a8b3610681fb588bada182a0b821b82668e34574
ryantsully <ryan.t.sullivan21@gmail.com>
2024-11-09 15:55:43 -0500

    Add basic structure of js and css

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My Web App</h1>
+            <p>This is a basic setup with Flask and React.</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+console.log("JavaScript file loaded!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("root").innerHTML = "<h1>Welcome to My Web App</h1>";
+});
