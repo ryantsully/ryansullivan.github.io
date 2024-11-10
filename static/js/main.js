8d6835dc32aac6ed6342266a79e06ae0597269aa
ryantsully <ryan.t.sullivan21@gmail.com>
2024-11-10 09:30:38 -0500

    Remove main.js

--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-console.log("JavaScript file loaded!");
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("root").innerHTML = "<h1>Welcome to My Web App</h1>";
-});
